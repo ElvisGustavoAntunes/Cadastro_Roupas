46c7f6ff169229ba5242f1bd4f31f173f870127a
Gustavinho <elvisgustavoantunes@gmail.com>
2023-05-08 21:32:10 -0300

    Funcao de logar

--- JavaScript/Login.js
+++ JavaScript/Login.js
@@ -1,1 +1,12 @@
-alert('Login');
+function logar(){
+    var usuario = document.getElementById("login");
+    var senha = document.getElementById("senha");
+
+    console.log(usuario.value + senha.value);
+
+    if(usuario.value == "admin" && senha.value == "admin"){
+        localStorage.setItem("acesso", true);
+
+        window.location.href = "Cadastro.html";
+    }
+}
