19f23e04b921d269f6d1e731f60b760f6799e3dc
Gustavinho <elvisgustavoantunes@gmail.com>
2023-05-09 19:56:24 -0300

    armazenando os dados

--- JavaScript/Dados.js
+++ JavaScript/Dados.js
@@ -1,1 +1,18 @@
-alert("Teste");
+function cadatrar(){
+    var descricao = document.getElementById("desc_Item");
+    var tamanho = document.getElementById("tamanho");
+    var quantidade = document.getElementById("quantidade");   
+    var referencia = document.getElementById("referencia");
+    var valor_custo = document.getElementById("valor_custo");
+    var valor_venda = document.getElementById("valor_venda");
+    var observação = document.getElementById("observação");
+
+    console.log(descricao.value);
+    console.log(tamanho.value);
+    console.log(quantidade.value);
+    console.log(referencia.value);
+    console.log(valor_custo.value);
+    console.log(valor_venda.value);
+    console.log(observação.value);
+  
+}
