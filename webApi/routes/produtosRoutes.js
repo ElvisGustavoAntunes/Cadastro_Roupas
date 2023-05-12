d47d092a1a418f053099a1411b1aede862bfabdb
Gustavinho <elvisgustavoantunes@gmail.com>
2023-05-11 23:10:56 -0300

    Criando rotas de consulta

--- webApi/routes/produtosRoutes.js
+++ webApi/routes/produtosRoutes.js
@@ -4,7 +4,6 @@ const Produto = require('../models/Produto');
 //create Cadastrando
 router.post('/', async (req,res) =>{
     const {descricao,tamanho,quantidade,referencia,valorCusto,valorVenda,observacao} = req.body
-
     if(!descricao){
         res.status(422).json({erro: 'A descricao do produto é obrigatoria'})
     }
@@ -17,7 +16,6 @@ router.post('/', async (req,res) =>{
     if(!quantidade){
         res.status(422).json({erro: 'A referencia do produto é obrigatoria'})
     }
-
     const produto = {
         descricao,
         tamanho,
@@ -27,14 +25,32 @@ router.post('/', async (req,res) =>{
         valorVenda,
         observacao
     }
-
     try{
         await Produto.create(produto)
         res.status(201).json({message : 'Produto Cadastrado com Sucesso'})
-
     }catch(error){
         res.status(500).json({error: error})
     }
 })
+//GET Leitura dos Dados
+router.get('/', async (req,res) =>{
 
+    try{
+        const produtos = await Produto.find()
+        res.status(200).json(produtos)
+    }catch(error){
+        res.status(500).json({error: error})
+    }
+})
+router.get('/:id', async (req,res) =>{
+    const id = req.params.id;
+
+    try{
+        const produtos = await Produto.findOne({_id: id})
+        res.status(200).json(produtos)
+    }catch(error){
+        res.status(500).json({error: error})
+    }
+
+})
 module.exports = router;
