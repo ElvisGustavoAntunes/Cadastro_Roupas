a11dbd7d6d3f9a35d95c17315f044feb56550bfe
Gustavinho <elvisgustavoantunes@gmail.com>
2023-05-11 23:18:52 -0300

    ajustando respostas da API

--- webApi/routes/produtosRoutes.js
+++ webApi/routes/produtosRoutes.js
@@ -6,15 +6,18 @@ router.post('/', async (req,res) =>{
     const {descricao,tamanho,quantidade,referencia,valorCusto,valorVenda,observacao} = req.body
     if(!descricao){
         res.status(422).json({erro: 'A descricao do produto é obrigatoria'})
+        return;
     }
     if(!tamanho){
         res.status(422).json({erro: 'O tamanho do produto é obrigatorio'})
     }
     if(!quantidade){
         res.status(422).json({erro: 'A quantidade do produto é obrigatoria'})
+        return;
     }
     if(!quantidade){
         res.status(422).json({erro: 'A referencia do produto é obrigatoria'})
+        return;
     }
     const produto = {
         descricao,
@@ -47,6 +50,11 @@ router.get('/:id', async (req,res) =>{
 
     try{
         const produtos = await Produto.findOne({_id: id})
+        if(!produtos){
+            res.status(422).json('{Mensage: Produto não Cadastrado }');
+            return;
+        }
+        
         res.status(200).json(produtos)
     }catch(error){
         res.status(500).json({error: error})
